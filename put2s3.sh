797b5e2a4a5d6dbf15f80a369dfeab372a75cd80
ZackZhouHB <zhbsoftboy1@gmail.com>
2023-03-09 20:24:35 +1100

    update 3 20230309

--- put2s3.sh
+++ put2s3.sh
@@ -2,18 +2,20 @@
 
 # ensure ansible and ssh and ssh key is ready
 # check my notes, here might need some extra tasks
+# install aws cli
+# asw configure with KEY ID and key secret to connect to S3
 
-# run playbook to backup all nas config files to local
-ansible-playbook naslocalbk.yml
+# Run playbook to backup all nas config files to local
+ansible-playbook nasbkupg.yml
 
-# reboot nas after upgrade
+# Reboot nas after upgrade
 ansible newnas -a "reboot"
-
-# here might need some extra step to install aws cli and aws config for key 
 
 # Upload the folder to S3
 aws s3 cp --recursive /root/nasbk s3://zackzs3001/nasbk/
-#wait reboot
+
+# Wait 2m for nas reboot after upgrade
 sleep 2m
-# out put nas version
-ansible-playbook naslocalreboot.yml
+
+# Output nas version
+ansible-playbook nasversion.yml
