dab1050b19f590dcbaf0f112f9cbccc2c2353624
ZackZhouHB <zhbsoftboy1@gmail.com>
2023-03-09 20:08:18 +1100

    update 20230309

--- put2s3.sh
+++ put2s3.sh
@@ -6,7 +6,14 @@
 # run playbook to backup all nas config files to local
 ansible-playbook naslocalbk.yml
 
+# reboot nas after upgrade
+ansible newnas -a "reboot"
+
 # here might need some extra step to install aws cli and aws config for key 
 
 # Upload the folder to S3
 aws s3 cp --recursive /root/nasbk s3://zackzs3001/nasbk/
+#wait reboot
+sleep 2m
+# out put nas version
+ansible-playbook naslocalreboot.yml
