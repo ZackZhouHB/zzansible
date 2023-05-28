561bcca442893e12796edba2d6b14a0bc7cc1ca0
zackz <zhbsoftboy1@gmail.com>
2023-05-28 22:43:49 +1000

    4 install script

--- provision-stack/files/install4.sh
+++ provision-stack/files/install4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install curl gnupg apt-transport-https -y
+sudo apt-get install curl gnupg apt-transport-https -y
 
 curl -1sLf "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/com.rabbitmq.team.gpg > /dev/null
 curl -1sLf "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xf77f1eda57ebb1cc" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg > /dev/null
@@ -11,11 +11,11 @@ deb-src [signed-by=/usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg] ht
 deb [signed-by=/usr/share/keyrings/io.packagecloud.rabbitmq.gpg] https://packagecloud.io/rabbitmq/rabbitmq-server/ubuntu/ jammy main
 deb-src [signed-by=/usr/share/keyrings/io.packagecloud.rabbitmq.gpg] https://packagecloud.io/rabbitmq/rabbitmq-server/ubuntu/ jammy main
 
-apt-get update -y
-apt-get install -y erlang-base \
+sudo apt-get update -y
+sudo apt-get install -y erlang-base \
     erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
     erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
     erlang-runtime-tools erlang-snmp erlang-ssl \
     erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl
 
-apt-get install rabbitmq-server -y --fix-missing
+sudo apt-get install rabbitmq-server -y --fix-missing
