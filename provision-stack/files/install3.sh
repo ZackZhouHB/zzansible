2430bc6ba21e755279977728bd7304e9899705db
zackz <zhbsoftboy1@gmail.com>
2023-05-28 22:35:59 +1000

    rabbitmq 4

--- provision-stack/files/install3.sh
+++ provision-stack/files/install3.sh
@@ -4,8 +4,7 @@ sudo apt update -y
 sudo apt install curl software-properties-common apt-transport-https lsb-release -y
 curl -fsSL https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/erlang.gpg
 
-echo "deb https://packages.erlang-solutions.com/ubuntu $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/erlang.list
-
+sudo apt update
 sudo apt install erlang -y
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.deb.sh | sudo bash
 
